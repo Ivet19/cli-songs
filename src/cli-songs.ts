f443cdf9101b541387b0bf57d4269b0a98155f88
Ivet Lopez <ivet19@live.com>
2025-02-12 11:55:53 +0100

    Primera entrega del proyecto con Array y condicional switch

--- src/cli-songs.ts
+++ src/cli-songs.ts
@@ -1,1 +1,29 @@
 import askUser from "./askUser.js";
+
+let songsNames: string[];
+songsNames = [
+  "Chihiro",
+  "Happier than ever",
+  "Birds of a feather",
+  "Wildflower",
+];
+
+let song: string;
+song = askUser("Introduce la posición de la canción que deseas escuchar: ");
+
+switch (song) {
+  case "1":
+    console.log(songsNames[0]);
+    break;
+  case "2":
+    console.log(songsNames[1]);
+    break;
+  case "3":
+    console.log(songsNames[2]);
+    break;
+  case "4":
+    console.log(songsNames[3]);
+    break;
+  default:
+    console.log("No existe ninguna canción en esa posición.");
+}
